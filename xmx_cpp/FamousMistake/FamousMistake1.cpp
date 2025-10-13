633d811fcb34ef64a10310050174322c14b6404b
stayfoolish <rh3295936059@outlook.com>
2025-10-13 10:47:04 +0800

    update FamousMistake

---
+++ xmx_cpp/FamousMistake/FamousMistake1.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char in;
+	cin>> in;
+	
+	if (in = 'Y')
+//Notice it's "=" rather than "=="
+		cout << "YES";
+	else
+		cout << "NO";
+
+
+
+return 0;
+
+}
--- xmx_cpp/README.md
+++ xmx_cpp/README.md
@@ -2,3 +2,4 @@
 
 - This is a code repo for my class "Fundamentals of Programming"
 - mostly my hw code, submitted to THU OJ
+- The professor is xmx(徐明星).
