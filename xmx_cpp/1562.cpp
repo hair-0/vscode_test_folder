0bc5087b0dab795dc6dd61c9aa7277fa11c5b5b9
stayfoolish <rh3295936059@outlook.com>
2025-10-28 11:22:29 +0800

    feat: add solution for problem 1562

---
+++ xmx_cpp/1562.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+using namespace std;
+
+bool checkSpecial(int i, int n)
+{
+	int origin_num = i;
+	int sum = 0;
+	int reversed_num = 0;
+
+	while (i > 0)
+	{
+		int digit = i % 10;
+		sum += digit;
+		reversed_num = reversed_num*10 + digit;
+		i /= 10;
+	}
+
+	if (sum == n && origin_num == reversed_num)
+	{
+		return true;
+	
+	}
+	return false;
+
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 10000; i < 1000000; i++)
+	{
+		if (checkSpecial(i,n))
+		{
+			cout << i << endl;
+		}
+
+	}
+
+	return 0;
+
+}
+
