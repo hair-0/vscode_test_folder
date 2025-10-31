1e47ca7235055d2027546bdf0ab908e004cc9093
stayfoolish <rh3295936059@outlook.com>
2025-10-31 23:31:13 +0800

    feat: 2565.cpp bug remain to resolve, and the first jugde failed

--- xmx_cpp/2565.cpp
+++ xmx_cpp/2565.cpp
@@ -1,1 +1,25 @@
-#include
+#include<bits/stdc++.h>
+using namespace std;
+
+double Eval_Polynomial(const double a[], int n, double x){
+    int count = 0;
+    for (int i = 0; i < n + 1; i++){
+        count += a[i] * pow(x, i);
+    }
+
+    return count;
+}
+
+int main(){
+    double x;
+    cin >> x;
+    double a[6];
+    int n;
+    cin >> n;
+    for(int i = 0; i < n + 1; i ++) cin >> a[i];
+
+    double res = Eval_Polynomial(a, n, x);
+    cout << fixed << setprecision(3) << res << endl;
+
+    return 0;
+}
