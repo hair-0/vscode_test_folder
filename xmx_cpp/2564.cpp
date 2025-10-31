74e3e464ab1eb63b557c48322fe767ef5ba3072d
stayfoolish <rh3295936059@outlook.com>
2025-10-31 23:04:13 +0800

    feat: finish 2564.cpp calleda valley count

--- xmx_cpp/2564.cpp
+++ xmx_cpp/2564.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 int count_valley(const int a[], int n){
+    if (n < 3) return 0;
+    int valley_count = 0;
 
-
-
+    for (int i = 1; i < n - 1; i++) {
+        if (a[i] < a[i - 1] && a[i] < a[i + 1]) {
+            valley_count++;
+        }
+    }
+    return valley_count;
 }
 
 int main(){
