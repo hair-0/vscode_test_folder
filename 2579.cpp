1e2cf0d6516331805a105757cc7e2efa0cc1301d
stayfoolish <rh3295936059@outlook.com>
2025-11-04 21:00:33 +0800

    feat: make a file 2579.cpp, finish a half of source code

---
+++ 2579.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//
+int main(){
+    int M,N;
+    cin >> M >> N;
+
+    ///
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < N; j++){
+            cin >> original[i][j];
+        }
+    }
+
+    tr
+
+
+    
+    return 0;
+}
